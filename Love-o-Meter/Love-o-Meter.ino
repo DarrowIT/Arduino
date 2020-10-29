95afedcbf5e5bef00874c070147d83348e26b9d7
ddarrow93 <ddarrow@law.upenn.edu>
2020-10-28 23:29:18 -0400

    format

--- Love-o-Meter/Love-o-Meter.ino
+++ Love-o-Meter/Love-o-Meter.ino
@@ -1,9 +1,12 @@
+// A pair of useful constants
 const int sensorPin = A0;
 const float baselineTemp = 20.0;
 
 void setup(){
-  Serial.begin(9600); // open a serial port
+  // Open a serial port and initialize to the desired speed
+  Serial.begin(9600);
 
+  // Initialize the digital pin directions and turn off
   for(int pinNumber = 2; pinNumber<5; pinNumber++){
     pinMode(pinNumber, OUTPUT);
     digitalWrite(pinNumber, LOW);
@@ -11,45 +14,49 @@ void setup(){
 }
 
 void loop() {
+  // Read the temperature sensor
   int sensorVal = analogRead(sensorPin);
 
+  // Read the temperature sensor values to the computer
   Serial.print("Sensor Value: ");
   Serial.print(sensorVal);
 
-  // convert the ADC reading to voltage
+  // Convert the sensor reading to voltage
   float voltage = (sensorVal/1024.0) * 5.0;
-
   Serial.print(", Volts: ");
   Serial.print(voltage);
 
+  // Convert the voltage to temperature and send the value to the computer
   Serial.print(", degrees C: ");
-  // convert the temperature in degrees
   float temperature = (voltage - .5) * 100;
   Serial.println(temperature);
 
+  // Turn off LEDs for a low temperature
   if(temperature < baselineTemp+2){
     digitalWrite(2, LOW);
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
     }
-    
+
+  // Turn on one LED for low-mid temperature  
   else if(temperature >= baselineTemp+2 && temperature < baselineTemp+4){
     digitalWrite(2, HIGH);
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
     }
-    
+
+  // Turn on two LEDs for mid-high temperature 
   else if(temperature >= baselineTemp+4 && temperature < baselineTemp+6){
     digitalWrite(2, HIGH);
     digitalWrite(3, HIGH);
     digitalWrite(4, LOW);
     }
-    
+
+  // Turn on three LEDs for a high temperature  
   else if(temperature >= baselineTemp+6){
     digitalWrite(2, HIGH);
     digitalWrite(3, HIGH);
     digitalWrite(4, HIGH);
     }
-
-    delay(1);
+  delay(1);
 }
