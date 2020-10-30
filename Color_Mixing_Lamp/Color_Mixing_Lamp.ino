5b020978c1665c51f599c029ea4ff55b153acc37
ddarrow93 <ddarrow@law.upenn.edu>
2020-10-29 22:52:18 -0400

    format cleanup

--- Color_Mixing_Lamp/Color_Mixing_Lamp.ino
+++ Color_Mixing_Lamp/Color_Mixing_Lamp.ino
@@ -1,34 +1,36 @@
+// Useful constants
 const int greenLEDPin = 9;
 const int redLEDPin = 10;
 const int blueLEDPin = 11;
-
 const int redSensorPin = A0;
 const int greenSensorPin = A1;
 const int blueSensorPin = A2;
 
+// Variables to store the sensor readings as well as the light level of each LED
 int redValue = 0;
 int greenValue = 0;
 int blueValue = 0;
-
 int redSensorValue = 0;
 int greenSensorValue = 0;
 int blueSensorValue = 0;
 
 void setup() {
+  // Setting the direction of the digital pins and setting up the serial port
   Serial.begin(9600);
-
   pinMode(greenLEDPin, OUTPUT);
   pinMode(redLEDPin, OUTPUT);
   pinMode(blueLEDPin, OUTPUT);
 }
 
 void loop() {
+  // Reading the value of each light sensor
   redSensorValue = analogRead(redSensorPin);
   delay(5);
   greenSensorValue = analogRead(greenSensorPin);
   delay(5);
   blueSensorValue = analogRead(blueSensorPin);
 
+  // Report the sensor readings to the computer
   Serial.print("Raw Sensor Values \t red: ");
   Serial.print(redSensorValue);
   Serial.print("\t green: ");
@@ -36,10 +38,12 @@ void loop() {
   Serial.print("\t blue: ");
   Serial.println(blueSensorValue);
 
+  // Converting the sonsor readings
   redValue = redSensorValue/4;
   greenValue = greenSensorValue/4;
   blueValue = blueSensorValue/4;
 
+  // Report the calculated LED light levels
   Serial.print("Mapped Sensor Values \t red: ");
   Serial.print(redValue);
   Serial.print("\t green: ");
@@ -47,6 +51,7 @@ void loop() {
   Serial.print("\t blue: ");
   Serial.println(blueValue);
 
+  // Set the LED light levels
   analogWrite(redLEDPin, redValue);
   analogWrite(greenLEDPin, greenValue);
   analogWrite(blueLEDPin, blueValue);
