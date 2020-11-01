4a35bd2af0012309f16b5a190afe5caed37f946d
ddarrow93 <ddarrow@law.upenn.edu>
2020-10-31 23:07:52 -0400

    Create Motorized_Pinwheel.ino

---
+++ Motorized_Pinwheel/Motorized_Pinwheel.ino
@@ -0,0 +1,14 @@
+// Name your constants and variables
+const int switchPin = 2;
+const int motorPin = 9;
+int switchState = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
