35e572389400e72ce7c958bb201545908623485f
ddarrow93 <ddarrow@law.upenn.edu>
2020-11-05 05:55:39 -0500

    Create Touchy-feely_Lamp.ino

---
+++ Touchy-feely_Lamp/Touchy-feely_Lamp.ino
@@ -0,0 +1,27 @@
+// Import the CapacitiveSensor library
+#include <CapacitiveSensor.h>
+CapacitiveSensor capSensor = CapacitiveSensor(4, 2);
+
+// Set up the threshold
+int threshold = 1000;
+const int ledPin = 12;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // Sensing touch
+  long sensorValue = capSensor.capacitiveSensor(30);
+  Serial.println(sensorValue);
+
+  // Lamp control
+  if(sensorValue > threshold) {
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+  delay(10);
+}
